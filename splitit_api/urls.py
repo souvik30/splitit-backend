0879b73c4c7a55c4a6360b8717b7eb4f4379d4e6
Soham Mukherjee <rockstarsam1234@gmail.com>
2024-01-07 11:41:46 +0530

    Added url for auth

--- splitit_api/urls.py
+++ splitit_api/urls.py
@@ -26,5 +26,6 @@ urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('auth/', views.AuthViewSet.as_view(), name='auth'),
     path('', include(router.urls)),
 ]
